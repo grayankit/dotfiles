3fc9947e680f3083e27698fd70f55b79086da7df
Ankit Grai <ankigray@outlook.com>
2025-05-06 23:12:36 +0530

    tmux plugin install

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,5 +8,13 @@ if [ -f "$HOME/dotfiles/pkglist.txt" ]; then
   echo "[*] Restoring additional packages from pkglist.txt..."
   sudo pacman -S --needed - < "$HOME/dotfiles/pkglist.txt"
 fi
+echo "[*] Installing tmux plugins..."
+
+TPM_DIR="$HOME/.config/tmux/plugins/tpm"
+if [ ! -d "$TPM_DIR" ]; then
+  git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+else
+  echo "  - TPM already installed."
+fi
 
 echo "[✓] Installation complete. Now run ./scripts/setup.sh"
