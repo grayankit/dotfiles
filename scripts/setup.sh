5135a90024149ffe49f9a32725e548d694d78bed
Ankit Grai <ankigray@outlook.com>
2025-11-24 01:28:11 +0530

    feat: added yazi config files

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "[*] Cleaning up ~/.config for stow..."
 
-CONFIG_APPS=(hypr nvim waybar alacritty tmux wlogout ml4w rofi scripts niri)
+CONFIG_APPS=(hypr nvim waybar alacritty tmux wlogout ml4w rofi scripts niri yazi)
 
 for app in "${CONFIG_APPS[@]}"; do
   if [ -L "$HOME/.config/$app" ]; then
