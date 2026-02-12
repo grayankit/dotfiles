3b66ccd5a9a9ff3b67a28f25bcc4e6138a64db7e
Ankit Grai <ankigray@outlook.com>
2026-02-12 23:39:35 +0530

    fix: made the so that it will link the libraries

--- config/.config/nvim/lua/custom/keymaps.lua
+++ config/.config/nvim/lua/custom/keymaps.lua
@@ -2,30 +2,25 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "c", "cpp" },
 	callback = function()
 		vim.keymap.set("n", "<F5>", function()
-			vim.cmd("w") -- Save the file
+			vim.cmd("w")
 
 			local ft = vim.bo.filetype
-			-- Use gcc for C files, g++ for C++ files
-			local compiler = (ft == "c") and "gcc" or "g++"
-
 			local file = vim.fn.expand("%")
 			local out = vim.fn.expand("%:r")
 
-			-- We wrap the command in bash -c '...' to handle the && and ; logic safely
-			-- 1. Compile with warnings (-Wall)
-			-- 2. If successful (&&), run the output
-			-- 3. Always (;), pause at the end so you can read the output
-			local shell_cmd = string.format(
-				'bash -c "%s %s -o %s -Wall && ./%s; echo; read -p "Press Enter to exit...""',
-				compiler,
-				file,
-				out,
-				out
-			)
+			local compile_cmd
 
-			-- Open split and run the command
+			if ft == "c" then
+				compile_cmd = string.format("gcc %s -Wall -Wextra -lm -o %s", file, out)
+			else
+				compile_cmd = string.format("g++ %s -Wall -Wextra -o %s", file, out)
+			end
+
+			local shell_cmd =
+				string.format('bash -c "%s && ./%s; echo; read -p \\"Press Enter to exit...\\""', compile_cmd, out)
+
 			vim.cmd("split | term " .. shell_cmd)
-			vim.cmd("startinsert") -- Automatically switch to insert mode
+			vim.cmd("startinsert")
 		end, { buffer = true, desc = "Compile and run C/C++" })
 	end,
 })
