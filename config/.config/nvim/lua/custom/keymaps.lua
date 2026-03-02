273b0e106213af93e2753eb9f2f8c783b71645f2
Ankit Grai <ankigray@outlook.com>
2026-03-03 02:19:41 +0530

    just a useless change

--- config/.config/nvim/lua/custom/keymaps.lua
+++ config/.config/nvim/lua/custom/keymaps.lua
@@ -16,10 +16,14 @@ vim.api.nvim_create_autocmd("FileType", {
 				compile_cmd = string.format("g++ %s -Wall -Wextra -o %s", file, out)
 			end
 
-			local shell_cmd =
-				string.format('bash -c "%s && ./%s; echo; read -p \\"Press Enter to exit...\\""', compile_cmd, out)
+			local compile_result = vim.fn.system(compile_cmd)
+			if vim.v.shell_error ~= 0 then
+				vim.notify("Compilation failed: " .. compile_result, vim.log.levels.ERROR)
+				return
+			end
 
-			vim.cmd("split | term " .. shell_cmd)
+			local run_cmd = string.format("./%s", out)
+			vim.cmd("split | term " .. run_cmd)
 			vim.cmd("startinsert")
 		end, { buffer = true, desc = "Compile and run C/C++" })
 	end,
