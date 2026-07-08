ec14579594bec4d58e3dc3c8448b5b39e0abf58f
Ankit Grai <ankigray@outlook.com>
2026-07-08 10:44:38 +0530

    feat: added binds to move active workspace across monitors

--- config/.config/hypr/conf/binds.lua
+++ config/.config/hypr/conf/binds.lua
@@ -65,6 +65,10 @@ end
 hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
 hl.bind(mainMod .. " + mouse_up", hl.dsp.focus({ workspace = "e-1" }))
 
+-- Move active workspace to monitor
+hl.bind(SUPER_SHIFT .. " + Left", hl.dsp.workspace.move({ monitor = "l" }))
+hl.bind(SUPER_SHIFT .. " + Right", hl.dsp.workspace.move({ monitor = "r" }))
+
 -- Move/resize windows with mainMod + LMB/RMB and dragging
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
